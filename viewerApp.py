4760bf99486030d66e8366dbf410ca3dabb71a61
Anita Souv <anita.souv@gmail.com>
2017-04-19 12:33:58 -0700

    Two actors
    The .obj file and the sphere are added and rendered at the same time
    (on top pf each other)

--- viewerApp.py
+++ viewerApp.py
@@ -5,18 +5,30 @@ reader = vtkOBJReader();
 path = "./vanquish.obj";
 reader.SetFileName(path);
 reader.Update();
+
+#Create a sphere
+sphereSource =vtkSphereSource();
+sphereSource.SetCenter(0.0, 0.0, 0.0);
+sphereSource.SetRadius(1);
+sphereSource.Update();
  
 mapper = vtkPolyDataMapper();
-# mapper.SetInput(reader.RequestData())
 mapper.SetInputConnection(reader.GetOutputPort());
  
+mapper2 = vtkPolyDataMapper();
+mapper2.SetInputConnection(sphereSource.GetOutputPort());
+
+
 actor = vtkActor();
 actor.SetMapper(mapper);
+
+actor2 = vtkActor();
+actor2.SetMapper(mapper2);
 
 
 # create camera
 camera =vtkCamera ();
-camera.SetPosition(0, 0,100);
+camera.SetPosition(0, 0,4);
 camera.SetFocalPoint(0, 0, 0);
 
 # from lab 1, camera.py
@@ -31,7 +43,7 @@ renderWindowInteractor.SetRenderWindow(renderWindow);
 
 renderer.SetBackground(1,1,1); # Background color white
 renderer.AddActor(actor);
-
+renderer.AddActor(actor2);
 
 #render, start
 renderWindow.Render();
