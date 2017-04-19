692edf96bb07a022587054fe39a3f06383feddfc
Anita Souv <anita.souv@gmail.com>
2017-04-19 12:23:21 -0700

    viewerApp now can read .obj file
    hard coded the vanquish.obj file
    
    - can interact
    
    but set to be really zoomed out

--- viewerApp.py
+++ viewerApp.py
@@ -1,6 +1,19 @@
 from vtk import* 
 
 
+reader = vtkOBJReader();
+path = "./vanquish.obj";
+reader.SetFileName(path);
+reader.Update();
+ 
+mapper = vtkPolyDataMapper();
+# mapper.SetInput(reader.RequestData())
+mapper.SetInputConnection(reader.GetOutputPort());
+ 
+actor = vtkActor();
+actor.SetMapper(mapper);
+
+
 # create camera
 camera =vtkCamera ();
 camera.SetPosition(0, 0,100);
@@ -17,6 +30,9 @@ renderWindowInteractor =vtkRenderWindowInteractor();
 renderWindowInteractor.SetRenderWindow(renderWindow);
 
 renderer.SetBackground(1,1,1); # Background color white
+renderer.AddActor(actor);
+
 
 #render, start
 renderWindow.Render();
+renderWindowInteractor.Start();
