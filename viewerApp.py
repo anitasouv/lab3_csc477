93900fb9bf408b5d8eb7c9dbe3e4377172dc0ba1
Anita Souv <anita.souv@gmail.com>
2017-04-19 19:35:51 -0700

    Organized Previous
    Still has the .obj file, sphere, and axes but still clumped together.
    
    The code it more organized and commented

--- viewerApp.py
+++ viewerApp.py
@@ -1,37 +1,10 @@
 from vtk import* 
 
-
-reader = vtkOBJReader();
-path = "./vanquish.obj";
-reader.SetFileName(path);
-reader.Update();
-
-#Create a sphere
-sphereSource =vtkSphereSource();
-sphereSource.SetCenter(0.0, 0.0, 0.0);
-sphereSource.SetRadius(1);
-sphereSource.Update();
- 
-mapper = vtkPolyDataMapper();
-mapper.SetInputConnection(reader.GetOutputPort());
- 
-mapper2 = vtkPolyDataMapper();
-mapper2.SetInputConnection(sphereSource.GetOutputPort());
-
-
-actor = vtkActor();
-actor.SetMapper(mapper);
-
-actor2 = vtkActor();
-actor2.SetMapper(mapper2);
-
-
 # create camera
 camera =vtkCamera ();
-camera.SetPosition(0, 0,4);
+camera.SetPosition(0, 0, 4);
 camera.SetFocalPoint(0, 0, 0);
 
-# from lab 1, camera.py
 # Create a renderer, render window, and interactor
 renderer =vtkRenderer();
 renderer.SetActiveCamera(camera);
@@ -41,10 +14,56 @@ renderWindow.AddRenderer(renderer);
 renderWindowInteractor =vtkRenderWindowInteractor();
 renderWindowInteractor.SetRenderWindow(renderWindow);
 
-renderer.SetBackground(1,1,1); # Background color white
-renderer.AddActor(actor);
-renderer.AddActor(actor2);
+renderer.SetBackground(1,1,1); # Background
 
-#render, start
+
+
+# *****read obj file 
+reader = vtkOBJReader();
+filename = "./vanquish.obj";
+reader.SetFileName(filename);
+reader.Update();
+
+# put obj file data to mapper
+objFile = vtkPolyDataMapper();
+objFile.SetInputConnection(reader.GetOutputPort());
+
+# put mapper into an actor
+objActor = vtkActor();
+objActor.SetMapper(objFile);
+
+# render the obj Actor
+renderer.AddActor(objActor);
+
+
+
+#*********Create a sphere
+sphereSource =vtkSphereSource();
+sphereSource.SetCenter(0.0, 0.0, 0.0);
+sphereSource.SetRadius(1);
+sphereSource.Update();
+
+# throw data into mapper
+sphere_mapper = vtkPolyDataMapper();
+sphere_mapper.SetInputConnection(sphereSource.GetOutputPort());
+
+# create actor from mappers
+sphere_actor = vtkActor();
+sphere_actor.SetMapper(sphere_mapper);
+
+# render the Sphere Actor
+renderer.AddActor(sphere_actor);
+
+
+
+
+axes = vtkAxesActor();
+axes.SetTotalLength(2,2,2);
+
+renderer.AddActor(axes);
+
+
+
+# Render and start
 renderWindow.Render();
 renderWindowInteractor.Start();
